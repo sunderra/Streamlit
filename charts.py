ba53803d6cae9a7d84c63a94f7a408cceab1af6e
sunderra <sunderra@gmail.com>
2024-10-15 20:52:20 +0530

    Update charts.py

--- charts.py
+++ charts.py
@@ -6,3 +6,6 @@ rand = np.random.normal(1, 2, size=20)
 fig, ax = plt.subplots()
 ax.hist(rand, bins=15)
 st.pyplot(fig)
+
+df = pd.DataFrame(np.random.randn(10, 2), columns=['x', 'y'])
+st.line_chart(df)
