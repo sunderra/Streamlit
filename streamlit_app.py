2ad9706dbf6ebecc537573acf92182d259acb79a
sunderra <sunderra@gmail.com>
2024-10-15 20:26:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,12 @@ st.selectbox('Pick a fruit', ['Apple', 'Banana', 'Orange'])
 st.multiselect('Choose a planet', ['Jupiter', 'Mars', 'Neptune'])
 st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
 st.slider('Pick a number', 0, 50)
+
+
+st.number_input('Pick a number', 0, 10)
+st.text_input('Email address')
+st.date_input('Traveling date')
+st.time_input('School time')
+st.text_area('Description')
+st.file_uploader('Upload a photo')
+st.color_picker('Choose your favorite color')
