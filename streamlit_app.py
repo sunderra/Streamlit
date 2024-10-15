3142341147ee868299a980b9c9df97fd5ebdc51f
sunderra <sunderra@gmail.com>
2024-10-15 20:25:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,12 @@ st.subheader("This is the subheader")
 st.caption("This is the caption")
 st.code("x = 2021")
 st.latex(r''' a+a r^1+a r^2+a r^3 ''')
+
+
+st.checkbox('Yes')
+st.button('Click Me')
+st.radio('Pick your gender', ['Male', 'Female'])
+st.selectbox('Pick a fruit', ['Apple', 'Banana', 'Orange'])
+st.multiselect('Choose a planet', ['Jupiter', 'Mars', 'Neptune'])
+st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
+st.slider('Pick a number', 0, 50)
