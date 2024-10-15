5d2888e0a6ef5703bc40bbec669e96dfeabe5db8
sunderra <sunderra@gmail.com>
2024-10-15 20:12:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
-st.write("Hello World!!")
+st.title("This is the app title")
+st.header("This is the header")
+st.markdown("This is the markdown")
+st.subheader("This is the subheader")
+st.caption("This is the caption")
+st.code("x = 2021")
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
