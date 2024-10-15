ce1e4c6da82b565d11c5d398b47c316bfaf7eb70
sunderra <sunderra@gmail.com>
2024-10-15 20:40:55 +0530

    Update containers.py

--- containers.py
+++ containers.py
@@ -3,5 +3,4 @@ import streamlit as st
 st.sidebar.title("Sidebar Title")
 st.sidebar.markdown("This is the sidebar content")
 st.sidebar.button("Click Me")
-st.sidebar.button("Click Me")
 st.sidebar.radio("Pick your gender",["Male","Female"])
