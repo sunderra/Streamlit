5ad3240aba5397e77c19181867f59416d41d9384
sunderra <sunderra@gmail.com>
2024-10-30 20:50:40 +0530

    Update loader.py

--- loader.py
+++ loader.py
@@ -4,8 +4,16 @@ from io import StringIO
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-
-    st.write(uploaded_file.name)
+    if filename.endswith(".xlsx") :
+        st.write("Excel file : " + uploaded_file.name)
+        data = pd.read_excel(uploaded_file, header=None)
+        st.write(data)
+    elif filename.endswith(".csv"):
+        st.write("CSV file: " + uploaded_file.name)
+        data = pd.read_csv(uploaded_file, header=None)
+        st.write(data)
+    else:    
+        st.write("Unknown file type")
     # To read file as bytes:
     #bytes_data = uploaded_file.getvalue()
     #st.write(bytes_data)
@@ -19,5 +27,3 @@ if uploaded_file is not None:
     #st.write(string_data)
 
     # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file, header=None)
-    st.write(dataframe)
