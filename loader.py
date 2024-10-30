7a2d57c97609f6e41c7c11aeba575d20c8ed106e
sunderra <sunderra@gmail.com>
2024-10-30 20:41:29 +0530

    Update loader.py

--- loader.py
+++ loader.py
@@ -4,6 +4,8 @@ from io import StringIO
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
+
+    st.write(uploaded_file.name)
     # To read file as bytes:
     #bytes_data = uploaded_file.getvalue()
     #st.write(bytes_data)
